f5c2d9f92a7e52a140d1b06698459776fd8fc4ae
wen2203 <692453@student.inholland.nl>
2025-06-24 18:57:18 +0200

    Update genemate.py

--- genemate.py
+++ genemate.py
@@ -15,35 +15,3 @@ def fetch_gene_data(gene_name, organism):
         return fasta_data if fasta_data.strip() else None
     except Exception as e:
         return None
-
-def download_gene(gene_name, organism="Homo sapiens", max_results=1):
-    Entrez.email = "your.email@example.com"  # Vul je email in voor NCBI
-    
-    handle = Entrez.esearch(db="nucleotide", term=f"{gene_name}[Gene] AND {organism}[Organism]", retmax=max_results)
-    record = Entrez.read(handle)
-    ids = record.get("IdList", [])
-
-    if not ids:
-        print("No results found.\n")
-        return False  # Geen resultaten
-
-    output_dir = "/Users/rainy/Documentslocal/Lab_Navigator2/genes"
-    os.makedirs(output_dir, exist_ok=True)
-
-    for gene_id in ids:
-        fetch_handle = Entrez.efetch(db="nucleotide", id=gene_id, rettype="fasta", retmode="text")
-        fasta_data = fetch_handle.read()
-
-        if not fasta_data.strip():  # check lege data
-            print(f"Geen FASTA data gevonden voor ID {gene_id}\n")
-            return False
-
-        filename = f"{gene_name}_{gene_id}.fasta"
-        output_path = os.path.join(output_dir, filename)
-
-        with open(output_path, "w") as f:
-            f.write(fasta_data)
-
-        print(f"✅ Gene downloaded to: {output_path}\n")
-
-    return True  # Download succesvol
